79345fd60fcec0e986370ffb1a8e669d5d8fb1d5
DESKTOP-7NDHLRU\BETU <tung.nv@ktkt.edu.vn>
2022-06-23 16:14:34 +0700

    23062022

---
+++ TeduShop.Data/Repositories/ApplicationUserGroupRepository.cs
@@ -0,0 +1,17 @@
+﻿using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IApplicationUserGroupRepository : IRepository<ApplicationUserGroup>
+    {
+
+    }
+    public class ApplicationUserGroupRepository : RepositoryBase<ApplicationUserGroup>, IApplicationUserGroupRepository
+    {
+        public ApplicationUserGroupRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+
+        }
+    }
+}
